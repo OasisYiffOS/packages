61d5b18dc80110a187eb4e4c4aed2320489617ab
Evie Viau <evie@eviee.gay>
2023-03-05 23:30:54 -0800

    Switch gdm to sysusers

--- gdm/postinst.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-glib-compile-schemas /usr/share/glib-2.0/schemas
-
-# Make a user and group to run GDM
-groupadd -g 21 gdm
-useradd -c "GDM Daemon Owner" -d /var/lib/gdm -u 21 \
-        -g gdm -s /bin/false gdm
-passwd -ql gdm
