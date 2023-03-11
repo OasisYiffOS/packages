6939528177e8b5c86e9ec297c7488e6f5a45d70c
Evie Viau <evie@eviee.gay>
2023-03-10 20:38:57 -0800

    Migrate more packages to sysusers

--- openssh/postinst.sh
+++ openssh/postinst.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-groupadd -g 50 sshd        &&
-useradd  -c 'sshd PrivSep' \
-         -d /var/lib/sshd  \
-         -g sshd           \
-         -s /bin/false     \
-         -u 50 sshd
-
 ssh-keygen -A
