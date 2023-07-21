548718741b20cf66f47eab567bf276458a737807
Evie Viau <evie@eviee.gay>
2023-07-21 02:21:59 -0400

    Remove dangling cockpit/postinst.sh

--- cockpit/postinst.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-useradd --system --home-dir /nonexistent --no-create-home cockpit-ws
-useradd --system --home-dir /nonexistent --no-create-home cockpit-wsinstance
-
-chown root:cockpit-wsinstance  /usr/lib/cockpit/cockpit-session
-sudo chmod 4750 /usr/lib/cockpit/cockpit-session
